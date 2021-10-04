d24940f4de915293ff0461f690ec87aee1222608
WestRyanK <west.ryan.k@gmail.com>
2021-10-04 15:51:49 -0600

    Fix cv2 import
    In PyCharm at least, import cv2 needs to be changed to
    from cv2 import cv2 to fix warnings and allow code autocomplete

--- ryan/ryan_image_processor.py
+++ ryan/ryan_image_processor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from cv2 import cv2
 from image_processor_interface import ImageProcessorInterface
 from data_labelling.game_state import *
 
@@ -6,4 +7,5 @@ from data_labelling.game_state import *
 class RyanImageProcessor(ImageProcessorInterface):
     def extract_state_from_image(self, input_image: np.ndarray) -> GameState:
         pass
+
 
--- util/data_loader.py
+++ util/data_loader.py
@@ -1,4 +1,4 @@
-import cv2
+from cv2 import cv2
 from glob import glob
 import json
 import os
